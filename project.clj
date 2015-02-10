7925ab9d8223b52ca018383e7a1b6946f9e85e56
gseaton <greg.seaton@aptpath.com>
2015-02-10 13:03:26 -0800

    updated project.clj with :scm key; added .idea to .gitignore

--- project.clj
+++ project.clj
@@ -4,4 +4,6 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[codox "0.8.10"]]
+  :scm {:name "git"
+        :url "https://github.com/aptpath/clj-service-locator"}
   :dependencies [[org.clojure/clojure "1.5.1"]])
